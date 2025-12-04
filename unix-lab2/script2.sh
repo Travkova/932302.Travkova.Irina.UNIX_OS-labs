12ec78fb228ab4a06fd950ee89643726bb2affa9
Travkova <Travinka2005@mail.ru>
2025-12-04 16:34:55 +0700

    Update script2.sh

--- unix-lab2/script2.sh
+++ unix-lab2/script2.sh
@@ -9,26 +9,32 @@ touch "$LOCK_FILE"
 counter=0
 
 while true; do
-    filename=$(
+  {
        flock -x 9
        i=1
        while [ $i -le 999 ]; do
            free_name=$(printf "%03d" $i)
-           if [ ! -f "$SHARED_DIR/$free_name" ]; then
+           if [ ! -e "$SHARED_DIR/$free_name" ]; then
               counter=$((counter + 1))
-              echo "Container: $CONTAINER_ID, File: $counter" > "$SHARED_DIR/$free_name"
-              echo "$free_name"
-              break
+              tmp="$SHARED_DIR/$free_name.tmp"
+              echo "Container: $CONTAINER_ID, File: $counter" > "$tmp"
+              if mv "$tmp" "$SHARED_DIR/$free_name"; then
+                 filename="$free_name"
+                 echo "[$CONTAINER_ID] Created: $filename"
+                 break
+              else
+                 echo "Can't move $tmp to $SHARED_DIR/$free_name"
+                 rm -f "$tmp"
+              fi
            fi
            i=$((i+1))
        done
-    ) 9>"$LOCK_FILE"
+   } 9> "$LOCK_FILE"
    if [ -z "$filename" ]; then
       echo "[$CONTAINER_ID] No free filename found" >&2
       sleep 1
       continue
    fi
-   echo "[$CONTAINER_ID] Created: $filename"
    sleep 1
 
    if [ -f "$SHARED_DIR/$filename" ]; then
@@ -39,4 +45,3 @@ while true; do
    fi
    sleep 1
 done
-
